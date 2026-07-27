9f3f97c98557279e410093f971fa0f27b3049ecf
Amogh-007-Rin <amoghdath233@gmail.com>
2026-07-27 19:26:23 +0100

    feat: add MainPage and Sidebar dashboard components

---
+++ web/src/app/dashboard/_components/MainPage.tsx
@@ -0,0 +1,7 @@
+export default function MainPage() {
+    return (
+        <div className="main-bar w-[93%] h-full bg-red-400">
+            
+        </div>
+    );
+};
---
+++ web/src/app/dashboard/_components/Sidebar.tsx
@@ -0,0 +1,15 @@
+export default function Sidebar() {
+    return (
+        <div className="side-navigation w-[6%] h-full bg-[#333333] flex flex-col justify-center items-center p-2">
+            <div className="part-1 w-[90%] h-[10%]">
+
+            </div>
+            <div className="part-2 w-[90%] h-[65%]">
+
+            </div>
+            <div className="part-3 w-[90%] h-[25%]">
+
+            </div>
+        </div>
+    );
+};

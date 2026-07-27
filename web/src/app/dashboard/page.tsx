24f9b013b1be3e71cb5f4964b22023f4559dc907
Amogh-007-Rin <amoghdath233@gmail.com>
2026-07-27 19:26:25 +0100

    feat: integrate Sidebar and MainPage into dashboard layout

--- web/src/app/dashboard/page.tsx
+++ web/src/app/dashboard/page.tsx
@@ -1,7 +1,11 @@
+import MainPage from "./_components/MainPage";
+import Sidebar from "./_components/Sidebar";
+
 export default function DashboardPage() {
   return (
-    <div className="w-screen h-screen flex justify-center items-center bg-zinc-900">
-      <h1>Dashboard</h1>
+    <div className="dashboard w-screen h-screen flex justify-between items-center bg-black p-4">
+        <Sidebar/>
+        <MainPage/>
     </div>
   );
-}
+};

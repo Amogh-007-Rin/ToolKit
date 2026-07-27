7e35dae6c231f99a1d30c541176d15eb123d65d4
Amogh-007-Rin <amoghdath233@gmail.com>
2026-07-27 17:51:13 +0100

    refactor: restructure Appbar with three-section layout and color blocks

--- web/src/components/ui/Appbar.tsx
+++ web/src/components/ui/Appbar.tsx
@@ -3,9 +3,15 @@ import { signIn, signOut } from "next-auth/react"
 
 export default function Appbar() {
     return (
-        <div className="w-full h-[10%] bg-black">
-            <button onClick={() => signIn()}>Signin</button>
-            <button onClick={() => signOut()}>Sign out</button>
+        <div className="appbar w-full h-[10%] bg-black flex justify-evenly items-center">
+            <div className="part-1 w-[30%] h-full bg-red-300">
+            </div>
+            <div className="part-2 w-[30%] h-full bg-green-300">
+            </div>
+            <div className="part-3 w-[30%] h-full bg-yellow-300">
+                <button onClick={() => signIn()}>Signin</button>
+                <button onClick={() => signOut()}>Sign out</button>
+            </div>
         </div>
     )
 }

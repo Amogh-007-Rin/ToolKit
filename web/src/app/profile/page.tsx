31aeb950fa333e98e03e63a759ff4cb57e030015
Amogh-007-Rin <amoghdath233@gmail.com>
2026-07-27 17:51:15 +0100

    feat: add initial profile page with dark theme layout

---
+++ web/src/app/profile/page.tsx
@@ -0,0 +1,7 @@
+export default function profilePage(){
+    return(
+        <div className="w-screen h-screen bg-zinc-900 flex justify-center items-center">
+            <h1>Profile Page</h1>
+        </div>
+    );
+};

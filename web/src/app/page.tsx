f1649d1aab87bd382e14c7996b13e8ab4eebb4d2
Amogh-007-Rin <amoghdath233@gmail.com>
2026-08-01 22:25:50 +0100

    feat: show appbar on home page
    - replace preview tool collection card with appbar

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,17 +2,14 @@
 
 import { useState } from "react";
 import ToolCollectionCard from "./dashboard/_components/cards/ToolCollectionCard";
+import Appbar from "@/components/ui/Appbar";
 
 export default function Home() {
   const [isCardOpen, setIsCardOpen] = useState(true);
 
   return (
-    <div className="w-screen h-screen bg-background flex items-center justify-center">
-      <ToolCollectionCard
-        isOpen={isCardOpen}
-        onClose={() => setIsCardOpen(false)}
-        onSubmit={() => setIsCardOpen(false)}
-      />
+    <div className="w-screen h-screen bg-background">
+      <Appbar/>
     </div>
   );
 };

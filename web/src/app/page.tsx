51fb232988af9dbcfa89d5c1af024b6cfc48f262
Amogh-007-Rin <amoghdath233@gmail.com>
2026-08-01 20:06:21 +0100

    feat: preview tool collection card on home page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,9 +1,18 @@
-import Appbar from "@/components/ui/Appbar";
+'use client'
+
+import { useState } from "react";
+import ToolCollectionCard from "./dashboard/_components/cards/ToolCollectionCard";
 
 export default function Home() {
+  const [isCardOpen, setIsCardOpen] = useState(true);
+
   return (
-    <div className="w-screen h-screen bg-background">
-      <Appbar></Appbar>
+    <div className="w-screen h-screen bg-background flex items-center justify-center">
+      <ToolCollectionCard
+        isOpen={isCardOpen}
+        onClose={() => setIsCardOpen(false)}
+        onSubmit={() => setIsCardOpen(false)}
+      />
     </div>
   );
-}
+};

52a97ae5d9c2bfe33dade4b931cda9f8927c4417
Amogh-007-Rin <amoghdath233@gmail.com>
2026-08-14 05:51:14 +0100

    feat: add placeholder contact and policy pages

---
+++ web/src/app/contact/page.tsx
@@ -0,0 +1,7 @@
+export default function ContactPage(){
+    return(
+        <div className="bg-background w-screen h-screen flex items-center justify-center">
+            CONTACT PAGE
+        </div>
+    );
+};
---
+++ web/src/app/policy/page.tsx
@@ -0,0 +1,7 @@
+export default function PolicyPage(){
+    return(
+        <div className="bg-background w-screen h-screen flex items-center justify-center">
+            POLICY PAGE
+        </div>
+    );
+};

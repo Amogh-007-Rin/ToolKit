705fad344dc4d916acf4b343c7a667f9eaf0dd56
Amogh-007-Rin <amoghdath233@gmail.com>
2026-08-14 05:51:13 +0100

    feat: show app bar on home page above spinner

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -6,9 +6,12 @@ import Spinner from "@/components/ui/loaders/Spinner";
 export default function Home() {
 
   return (
-    <div className="w-screen h-screen bg-background flex items-center justify-center">
-      {/* <Appbar/> */}
-      <Spinner/>
+    <div className="w-screen h-screen bg-background ">
+      <Appbar/>
+      <div className="w-full h-[80%] flex items-center justify-center">
+        <Spinner/>
+      </div>
+      
     </div>
   );
 };
